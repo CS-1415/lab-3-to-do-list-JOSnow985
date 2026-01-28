339462b7e23093a260be1eb02890c968accfd6e7
JOSnow985 <jaden.olvera@students.snow.edu>
2026-01-27 17:32:07 -0700

    Added Task class with data members

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// Jaden Olvera, 1/27/26, Lab 3 - ToDo List
 Console.WriteLine("A ToDo List app!");
+
+class Task
+{
+    // Data members for a Task
+    private int _taskID;
+    private string _taskTitle = "";
+    private string _taskDescription = "";
+    private bool _taskComplete;
+}
--- obj/Debug/net8.0/lab-3-to-do-list-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-3-to-do-list-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-to-do-list-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c08fc062e233164e1e64b1b3b038b12809547b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bf3b709e87d629beab89f968e5acdba3a0257e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-to-do-list-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-to-do-list-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
